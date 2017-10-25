361ec7cc545b077aadabe2565d7da1f0b9339ffd
perak <petar.korponaic@gmail.com>
2017-10-25 13:11:53 +0200

    It works now

--- index.js
+++ index.js
@@ -1,79 +1,126 @@
 var MongoClient = require('mongodb').MongoClient;
-var assert = require('assert');
+var wait = require("wait.for");
 
 // Connection URL
 var url = 'mongodb://localhost:3001/meteor';
 
+var getSchema = function() {
+	var db = wait.forMethod(MongoClient, "connect", url);
+	console.log("Connected successfully to server");
+
+	var l = db.listCollections();
+	var collectionInfos = wait.forMethod(l, "toArray");
+
+	var schema = {};
+	var collections = {};
+
+	var findRelatedCollection = function(value, field) {
+
+		for(var collectionName in collections) {
+
+			var related = wait.forMethod(collections[collectionName].collection, "findOne", { _id: value });
+			if(related) {
+				field["foreignKey"] = true;
+				field["references"] = collectionName;
+			} else {
+				field["key"] = true;
+			}
+		}
+	};
+
+	var getDocSchema = function(doc, docSchema) {
+		for(var key in doc) {
+			if(!docSchema[key]) {
+				docSchema[key] = { "types": {} };
+			}
+
+			var typeName = typeof doc[key];
+			if(typeName === "object") {
+				typeName = Object.prototype.toString.call(doc[key]);
+			}
+
+			if(!docSchema[key]["types"][typeName]) {
+				docSchema[key]["types"][typeName] = { frequency: 0 };
+			}
+			docSchema[key]["types"][typeName]["frequency"]++;
 
 
+			if(typeName == "string" && /^[23456789ABCDEFGHJKLMNPQRSTWXYZabcdefghijkmnopqrstuvwxyz]{17}$/.test(doc[key])) {
+				if(key == "_id") {
+					docSchema[key]["primaryKey"] = true;
+				} else {
+					findRelatedCollection(doc[key], docSchema[key]);
+				}
+			}
 
-var getSchema = function(cb) {
-	MongoClient.connect(url, function(err, db) {
-		assert.equal(null, err);
-		console.log("Connected successfully to server");
+			if(typeName == "[object Object]") {
+				docSchema[key]["types"][typeName]["structure"] = {};
+				getDocSchema(doc[key], docSchema[key]["types"][typeName]["structure"]);
+			}
+		}
+	};
 
-
-		db.listCollections().toArray().then(function(collectionInfos) {
-
-			var schema = {};
-			var collections = {};
-
-			collectionInfos.map(function(collectionInfo, index) {
-				schema[collectionInfo.name] = { ___processed: 0 };
-				collections[collectionInfo.name] = {};
-				collections[collectionInfo.name]["collection"] = db.collection(collectionInfo.name);
-
-				collections[collectionInfo.name]["collection"].find({}, { limit: 100 }).toArray().then(function(docs) {
-					schema[collectionInfo.name]["___processed"] = docs.length;
-					docs.map(function(doc) {
-						for(var key in doc) {
-							if(!schema[collectionInfo.name][key]) {
-								schema[collectionInfo.name][key] = { types: {} };
-							}
-
-							var typeName = typeof doc[key];
-							if(typeName === "object") {
-								typeName = Object.prototype.toString.call(doc[key]);
-							}
-							schema[collectionInfo.name][key]["types"][typeName] = schema[collectionInfo.name][key]["types"][typeName] ? schema[collectionInfo.name][key]["types"][typeName] + 1 : 1;
+	var mostFrequentType = function(docSchema, processed) {
+		if(processed) {
+			for(var fieldName in docSchema) {
+				if(docSchema[fieldName]) {
+					var max = 0;
+					var notNull = true;
+					for(var typeName in docSchema[fieldName]["types"]) {
+						if(typeName == "[object Null]") {
+							notNull = false;									
 						}
-					});
-
-					var processed = schema[collectionInfo.name]["___processed"] || 0;
-					if(processed) {
-						for(var fieldName in schema[collectionInfo.name]) {
-							if(schema[collectionInfo.name][fieldName]) {
-								var max = 0;
-
-								for(var typeName in schema[collectionInfo.name][fieldName]["types"]) {
-									schema[collectionInfo.name][fieldName]["types"][typeName] = schema[collectionInfo.name][fieldName]["types"][typeName] / processed;
-									if(schema[collectionInfo.name][fieldName]["types"][typeName] > max) {
-										max = schema[collectionInfo.name][fieldName]["types"][typeName];
-										if(typeName != "undefined" && typeName != "[object Null]") {
-											schema[collectionInfo.name][fieldName]["type"] = typeName;
-										}
-									}
-								}
-
+						docSchema[fieldName]["types"][typeName]["frequency"] = docSchema[fieldName]["types"][typeName]["frequency"] / processed;
+						if(docSchema[fieldName]["types"][typeName]["frequency"] > max) {
+							max = docSchema[fieldName]["types"][typeName]["frequency"];
+							if(typeName != "undefined" && typeName != "[object Null]") {
+								docSchema[fieldName]["type"] = typeName;
 							}
 						}
 					}
-					delete schema[collectionInfo.name].___processed;
-
-					if(index + 1 == collectionInfos.length) {
-						db.close();
-						if(cb) {
-							cb(schema);
-						}
+					if(!docSchema[fieldName]["type"]) {
+						docSchema[fieldName]["type"] = "undefined";
+						notNull = false;
 					}
 
-				});
-			});
+					var dataType = docSchema[fieldName]["type"];
+					if(dataType == "[object Object]") {
+						mostFrequentType(docSchema[fieldName]["types"][dataType]["structure"], processed);
+						docSchema[fieldName]["structure"] = docSchema[fieldName]["types"][dataType]["structure"];
+					}
+					delete docSchema[fieldName]["types"];
+
+					docSchema[fieldName]["required"] = notNull;
+				}
+			}
+		}
+	};
+
+	collectionInfos.map(function(collectionInfo, index) {
+		var docSchema = {};
+		schema[collectionInfo.name] = docSchema;
+
+		var collectionData = {};
+		collections[collectionInfo.name] = collectionData;
+		collectionData["collection"] = db.collection(collectionInfo.name);
+
+		var cur = wait.forMethod(collectionData["collection"], "find", {}, { limit: 100 });
+		var docs = wait.forMethod(cur, "toArray");
+		docs.map(function(doc) {
+			getDocSchema(doc, docSchema);
 		});
+
+		mostFrequentType(docSchema, docs.length);
 	});
+
+	db.close();
+	return schema;
 };
 
 
-getSchema(function(schema) {
+var x = function() {
+	var schema = getSchema();
 	console.log(JSON.stringify(schema, null, 4));
-});
+};
+
+wait.launchFiber(x);
